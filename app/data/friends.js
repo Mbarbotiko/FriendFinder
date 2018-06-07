59a5b16a87220eabef8671dad7e2d919e5105b0a
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-06-07 11:59:04 -0500

    adding dummy data into friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+
+
+var friendArr = [
+    {
+      "name" : "Margarita",
+      "photo": "https://mbarbotiko.github.io/images/profile.jpg",
+      "scores": [5,5,5,5,5,5,5,5,5,5,]
+    }
+  ];
+  
+  module.exports = friendArr;
+  
---
+++ routing/apiroutes.js
@@ -0,0 +1,17 @@
+var friendsData = require("../app/data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+      });
+
+      app.post("/api/friends", function(req, res) {
+          var newFriend= req.body;
+          console.log(newFriend);
+          friendsData.push(newFriend);
+          res.json(newFriend);
+
+      });
+
+    };
+
