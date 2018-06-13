2d2248861affb8e337b9b3a8c55455e863f1ec96
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-06-13 15:29:46 -0500

    fixed friend match logic, added some start friends to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,25 @@
-
-
 var friendArr = [
-    {
-      name : "Margarita",
-      photo: "https://mbarbotiko.github.io/images/profile.jpg",
-      scores: [1,1,1,1,1,1,1,1,1,1]
-    }
-  ];
+  {
+    name: "Margarita",
+    photo: "https://mbarbotiko.github.io/images/profile.jpg",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Peter",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Peter_Griffin.png/220px-Peter_Griffin.png",
+    scores: [1, 4, 3, 1, 4, 5, 3, 3, 3, 1]
+  },
+  {
+    name: "Garfield",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Garfield_the_Cat.svg/1200px-Garfield_the_Cat.svg.pngg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Batman",
+    photo: "https://i.ytimg.com/vi/DKbkKJWYT6E/maxresdefault.jpg",
+    scores: [5, 2, 4, 3, 1, 1, 1, 4, 5, 3]
+  }
 
+];
 
-  module.exports = friendArr;
-
-  
+module.exports = friendArr;
--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -1,5 +1,3 @@
-
-
 var friendsData = require("../app/data/friends");
 var path = require('path');
 
@@ -9,33 +7,34 @@ module.exports = function (app) {
 
     });
 
-
-
     app.post("/api/friends", function (req, res) {
         var userData = req.body;
         var userResponses = userData.scores;
         var matchName = '';
         var matchImage = '';
-        var totalDifference = 10000;
+        var totalDifference = 100;
 
         for (var i = 0; i < friendsData.length; i++) {
             var difference = 0;
             for (var j = 0; j < userResponses.length; j++) {
                 difference += Math.abs(friendsData[i].scores[j] - userResponses[j]);
-                console.log(difference);
+                //Loop through friends object, loop through users response object.  Then using math.abs set the difference from 0 to friends scores minus new users scores. Now we have the absolute difference between the two.
+
             }
+            console.log('difference is for '+friendsData[i].name+' is '+difference);
+            //console logging the difference for each friend.
             if (difference < totalDifference) {
+                console.log(difference+" "+totalDifference);
                 totalDifference = difference;
                 matchName = friendsData[i].name;
                 matchImage = friendsData[i].photo;
+                //if match has lowest difference record  the match
             }
-
-            friendsData.push(userData);
-            res.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
+        
         }
-
-
-
+        friendsData.push(userData);
+        res.json({ status: 'OK', matchName: matchName, matchImage: matchImage })
+        console.log('Match for user is: '+matchName+' link for match is'+matchImage);
     });
 
 };
